86d0c081335a3ab2432d4aea90ce300d63868c1f
Варлевский Александр <varlevskii.as@phystech.edu>
2026-09-23 17:34:26 +0300

    Сделана п1.2.3

--- 123-led-button-debounce/main.c
+++ 123-led-button-debounce/main.c
@@ -5,15 +5,6 @@
 
 const uint LED_PIN = 25;
 const uint BUTTON_PIN = 15;
-const uint DEBOUNCE_MS = 20;
-
-bool get_button_debounce(uint pin)
-{
-    bool state = gpio_get(pin);
-    sleep_ms(DEBOUNCE_MS);
-    return state && gpio_get(pin);
-}
-
 int main(){
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -28,7 +19,7 @@ int main(){
 
      while (1)
     {
-        bool current = get_button_debounce(BUTTON_PIN);
+        bool current = gpio_get(BUTTON_PIN);
 
         if (previous == true && current == false)
         {
